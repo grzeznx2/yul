3d4ad302fba432495f571900fc4ba8aad594722f
grzeznx2 <grzeznx2@gmail.com>
2023-02-04 17:34:05 +0100

    Lint

--- contracts/Bitwise.sol
+++ contracts/Bitwise.sol
@@ -6,11 +6,8 @@ contract Bitwise {
     function countBitSet(uint8 data) public pure returns (uint8 result) {
 
         for( uint i = 0; i < 8; i += 1) {
-
             if( ((data >> i) & 1) == 1) {
-
                 result += 1;
-
             }
 
         }
